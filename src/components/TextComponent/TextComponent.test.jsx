3514104d854bd2a01f5213396cc52809b464a35e
Luiz Otavio <luizomf@gmail.com>
2021-02-20 12:12:43 -0300

    TextComponent - com testes

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -3,8 +3,23 @@ import { renderTheme } from '../../styles/render-theme';
 import { TextComponent } from '.';
 
 describe('<TextComponent />', () => {
-  it('should render', () => {
+  it('should render a text', () => {
     renderTheme(<TextComponent>Children</TextComponent>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByText('Children')).toBeInTheDocument();
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        font-size: 2.4rem;
+      }
+
+      <p
+        class="c0"
+      >
+        Children
+      </p>
+    `);
   });
 });
