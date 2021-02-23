a21c45fd7f88a917c7fa3a90c4736980001628d6
Luiz Otavio <luizomf@gmail.com>
2021-02-23 14:00:31 -0300

    [Menu com testes

--- src/components/Menu/Menu.test.jsx
+++ src/components/Menu/Menu.test.jsx
@@ -1,10 +1,63 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import { Menu } from '.';
 
+import linksMock from '../NavLinks/mock';
+import { theme } from '../../styles/theme';
+const logoData = {
+  text: 'Logo',
+  link: '#target',
+};
+
 describe('<Menu />', () => {
-  it('should render', () => {
-    renderTheme(<Menu>Children</Menu>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render Logo and Main Menu Nav', () => {
+    const { container } = renderTheme(
+      <Menu links={linksMock} logoData={logoData} />,
+    );
+    expect(screen.getByRole('heading', { name: 'Logo' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('navigation', { name: 'Main menu' }),
+    ).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render menu mobile and button for open and close the menu', () => {
+    const { container } = renderTheme(
+      <Menu links={linksMock} logoData={logoData} />,
+    );
+
+    const button = screen.getByLabelText('Open/Close menu');
+    const menuContainer = button.nextSibling;
+
+    expect(button).toHaveStyleRule('display', 'none');
+    expect(button).toHaveStyleRule('display', 'flex', {
+      media: theme.media.lteMedium,
+    });
+
+    expect(menuContainer).toHaveStyleRule('opacity', '0', {
+      media: theme.media.lteMedium,
+    });
+    expect(screen.getByLabelText('Open menu')).toBeInTheDocument();
+
+    fireEvent.click(button);
+    expect(menuContainer).toHaveStyleRule('opacity', '1', {
+      media: theme.media.lteMedium,
+    });
+    expect(screen.getByLabelText('Close menu')).toBeInTheDocument();
+
+    fireEvent.click(menuContainer);
+    expect(menuContainer).toHaveStyleRule('opacity', '0', {
+      media: theme.media.lteMedium,
+    });
+    expect(screen.getByLabelText('Open menu')).toBeInTheDocument();
+  });
+
+  it('should not render links', () => {
+    const { container } = renderTheme(<Menu logoData={logoData} />);
+    expect(
+      screen.queryByRole('navigation', { name: 'Main menu' }).firstChild,
+    ).not.toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 });
