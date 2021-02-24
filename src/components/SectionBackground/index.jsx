bfe283642c66f4406866948fd457e6f65e708843
Luiz Otavio <luizomf@gmail.com>
2021-02-24 14:20:03 -0300

    Correção de IDs aleatórios

--- src/components/SectionBackground/index.jsx
+++ src/components/SectionBackground/index.jsx
@@ -2,18 +2,13 @@ import P from 'prop-types';
 import { SectionContainer } from '../SectionContainer';
 import * as Styled from './styles';
 
-const random = () =>
-  `id-${Math.random() * 10000}`.replace(/[^a-z0-9-_]/gi, '-');
-
 export const SectionBackground = ({
   children,
   background = false,
   sectionId = '',
 }) => {
-  const id = sectionId ? sectionId : random();
-
   return (
-    <Styled.Container background={background} id={id}>
+    <Styled.Container background={background} id={sectionId}>
       <SectionContainer>{children}</SectionContainer>
     </Styled.Container>
   );
